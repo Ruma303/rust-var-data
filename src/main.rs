9565574ae698163dddccd9dcffc59485fcfbb6a6
Ruma <matteoh8@hotmail.it>
2023-08-13 23:17:23 +0200

    rust-var-data

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+//% Variabili
+fn main() {
+
+    //$ Dichiarazione di variabili
+    /* let x = 10;
+    x = 20; //. Errore */
+
+    //$ Mutabilità e riassegnazione
+    /* let mut y = 10;
+    println!("Il valore di y è: {y}");
+    y = 20;
+    println!("Il valore di y è: {y}"); */
+
+    //$ Stampare i valori di variabili
+    /* let x = 10;
+    let y = 10.123456;
+    let z = "Hello World";
+
+    println!("x = {x}"); // x = 10
+    println!("x = {}", x); // x = 10
+    println!("y = {:.2}", y); // y = 10.12
+    println!("z = {:?}", z); // z = "Hello World"
+    println!("y = {:e}", y); // y = 1.012346e1
+    println!("x = {:b}", x); // x = 1010
+    println!("x = {:X}, y = {:.10}, z = {:?}", x, y, z); */
+    //x = A, y = 10.1234560000, z = "Hello World"
+
+}
